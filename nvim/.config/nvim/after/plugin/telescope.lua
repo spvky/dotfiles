9a790c30b157106ec7a22c689db49a04703a5aec
spooky-th-ghost <colin@wildheartinteractive.com>
2024-10-11 15:25:15 -0400

    Show hidden files in git files as well

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -4,3 +4,12 @@ vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fs', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+local telescope = require('telescope')
+telescope.setup {
+	pickers = {
+		find_files = {
+			hidden = true
+		};
+	}
+}

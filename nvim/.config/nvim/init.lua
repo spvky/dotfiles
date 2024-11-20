4bb376ef4b8a38ebbe1f12fd5dfe47b877447269
spooky-th-ghost <colin@wildheartinteractive.com>
2024-11-20 12:26:13 -0500

    added picom config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,6 @@
 require("spvky")
 require('onedark').load()
 vim.opt.termguicolors = true
--- require("bufferline").setup{}
 vim.opt.rnu = true
 vim.opt.autoindent = true
 vim.opt.tabstop = 2
@@ -10,7 +9,6 @@ vim.opt.smarttab = true
 vim.opt.softtabstop = 2
 vim.opt.hidden = true
 vim.opt.list = true
--- Case insensitive searching
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.g.rustfmt_autosave = 1

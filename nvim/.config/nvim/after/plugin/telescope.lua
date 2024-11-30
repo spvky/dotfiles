b1cb7f33c76c87aafe0e613ea73e03442a3b4bef
spvky <Colin@wildheartinteractive.com>
2024-11-30 13:45:41 -0500

    test

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -3,6 +3,7 @@ vim.keymap.set('n', '<leader>f', builtin.git_files, {})
 vim.keymap.set('n', '<leader>a', builtin.find_files, {})
 vim.keymap.set('n', '<leader>s', builtin.live_grep, { desc = 'Telescope live grep' })
 
+
 local telescope = require('telescope')
 telescope.setup {
 	pickers = {

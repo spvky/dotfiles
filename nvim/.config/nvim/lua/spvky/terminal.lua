0fbb093f6e23aa1da2464576bd91cb36e0754e7f
spooky-th-ghost <colin@wildheartinteractive.com>
2024-12-17 14:04:10 -0500

    kitty change

--- nvim/.config/nvim/lua/spvky/terminal.lua
+++ nvim/.config/nvim/lua/spvky/terminal.lua
@@ -9,12 +9,22 @@ vim.api.nvim_create_autocmd('TermOpen', {
 -- Remap exit terminal mode to be less annoying
 vim.keymap.set('t', '<Esc>', "<C-\\><C-n>", {silent = true})
 
-local job_id = 0
+local short_terminal_id = 0
 -- Opens a 10 row high terminal
-vim.keymap.set('n', '<leader>st', function()
+vim.keymap.set('n', '<leader>th', function()
 	vim.cmd.vnew()
 	vim.cmd.term()
 	vim.cmd.wincmd('J')
 	vim.api.nvim_win_set_height(0,10)
 	job_id = vim.bo.channel
+end)
+
+local vertical_terminal_id = 1
+-- Opens a 10 row high terminal
+vim.keymap.set('n', '<leader>tv', function()
+	vim.cmd.new()
+	vim.cmd.term()
+	vim.cmd.wincmd('L')
+	vim.api.nvim_win_set_width(0,50)
+	vertical_terminal_id = vim.bo.channel
 end)

30c0443cfa747e8a45c964c3cd29d23c0561c80b
Colin R. Casto <Colin@wildheartinteractive.com>
2026-01-06 21:57:13 -0500

    when opening with no specified file, open nvim in Oil

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -539,3 +539,13 @@ require('lazy').setup {
     },
   },
 }
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = vim.schedule_wrap(function(data)
+		vim.print(vim.fn.isdirectory(data.file))
+		if data.file == "" or vim.fn.isdirectory(data.file) ~= 0 then
+			vim.print(data.file)
+			require("oil").open()
+		end
+	end),
+})
